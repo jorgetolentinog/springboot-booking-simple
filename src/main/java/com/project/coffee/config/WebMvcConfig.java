9244c6d728cc9efd23491498545605e9326d0b61
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2022-07-17 18:53:32 -0500

    -

---
+++ src/main/java/com/project/coffee/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.project.coffee.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig {
+
+    @Bean
+    WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+}
--- src/main/java/com/project/coffee/repository/mock/BookingRepositoryMock.java
+++ src/main/java/com/project/coffee/repository/mock/BookingRepositoryMock.java
@@ -1,5 +1,6 @@
 package com.project.coffee.repository.mock;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -13,59 +14,53 @@ import com.project.coffee.repository.BookingRepository;
 @Service
 @Primary
 public class BookingRepositoryMock implements BookingRepository {
+    private List<Booking> bookings = new ArrayList<>();
+
+    public BookingRepositoryMock() {
+        Booking booking = new Booking();
+        booking.setId(1L);
+        booking.setTableId(1L);
+        booking.setUserId(1L);
+        booking.setDate(new Date());
+        booking.setGuests(2);
+        booking.setStateId(BookingState.PENDING);
+        bookings.add(booking);
+
+        Booking booking2 = new Booking();
+        booking2.setId(2L);
+        booking2.setTableId(2L);
+        booking2.setUserId(1L);
+        booking2.setDate(new Date());
+        booking2.setGuests(3);
+        booking2.setStateId(BookingState.CONFIRMED);
+        bookings.add(booking2);
+    }
 
     @Override
     public Booking save(Booking booking) {
         if (booking.getId() == null) {
-            booking.setId(2000L);
+            booking.setId(new Date().getTime());
+            bookings.add(booking);
         }
-    
+
         return booking;
     }
 
     @Override
     public List<Booking> findAllByUserId(Long userId) {
-        Booking booking = new Booking();
-        booking.setId(1L);
-        booking.setTableId(1L);
-        booking.setUserId(1L);
-        booking.setDate(new Date());
-        booking.setGuests(2);
-        booking.setStateId(BookingState.PENDING);
-
-        Booking booking2 = new Booking();
-        booking2.setId(1L);
-        booking2.setTableId(1L);
-        booking2.setUserId(1L);
-        booking2.setDate(new Date());
-        booking2.setGuests(2);
-        booking2.setStateId(BookingState.CONFIRMED);
-
-        return List.of(booking, booking2);
+        return bookings;
     }
 
     @Override
     public Booking findById(Long id) {
-        Booking booking = new Booking();
-        booking.setId(1L);
-        booking.setTableId(1L);
-        booking.setUserId(1L);
-        booking.setDate(new Date());
-        booking.setGuests(2);
-        booking.setStateId(BookingState.PENDING);
-        return booking;
+        return bookings.stream()
+                .filter(booking -> booking.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public List<Booking> findAll() {
-        Booking booking = new Booking();
-        booking.setId(1L);
-        booking.setTableId(1L);
-        booking.setUserId(1L);
-        booking.setDate(new Date());
-        booking.setGuests(2);
-        booking.setStateId(BookingState.PENDING);
-
-        return List.of(booking);
+        return bookings;
     }
 }
