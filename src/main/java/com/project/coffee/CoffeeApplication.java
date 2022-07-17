24325814899d0e1fec0c53ca793e8a1062574d6e
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2022-07-17 15:14:28 -0500

    -

--- src/main/java/com/project/coffee/CoffeeApplication.java
+++ src/main/java/com/project/coffee/CoffeeApplication.java
@@ -2,10 +2,9 @@ package com.project.coffee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.validation.annotation.Validated;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 @Validated
 public class CoffeeApplication {
 
